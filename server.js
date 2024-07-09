27a248ae1c98ce7789bbea1390e12e95295d0481
Rajnish Kumar <143951999+rajnish-bytes@users.noreply.github.com>
2024-07-09 20:24:27 +0000

    changes

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 
+app.get("/", (req, res)=>{
+    res.json({ data: "Retrive data from server"})
+})
+
 app.get("/api", (req, res)=>{
     res.json({ data: "Retrive data from server"})
 })
+
 app.listen(()=>{
     console.log(`server running at ${PORT}`);
 })
