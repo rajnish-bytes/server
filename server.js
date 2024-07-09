b280f086e9a5f118c559042446e80852850150bd
Rajnish Kumar <143951999+rajnish-bytes@users.noreply.github.com>
2024-07-09 20:19:35 +0000

    Added server file

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+
+app.get("/api", (req, res)=>{
+    res.json({ data: "Retrive data from server"})
+})
+app.listen(()=>{
+    console.log(`server running at ${PORT}`);
+})
